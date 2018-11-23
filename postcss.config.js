5f1c9fe0088217460a889dd304355d8f5a0be348
Lionel T <elrumordelaluz@hotmail.com>
2018-11-23 10:13:54 +0100

    Add demo html and plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   plugins: [
     'postcss-import',
+    'postcss-nested',
     'postcss-custom-media',
     'postcss-custom-properties',
     'postcss-calc',
     'postcss-discard-comments',
     'postcss-remove-root',
     'autoprefixer',
+    'css-mqpacker',
     'postcss-reporter',
     './src/plugins/test',
   ].map(require),
